dd2dd0cafc396bcf24f33be5194cae9aec414176
LauraWaldron <lwaldron2021@fau.edu>
2023-06-08 14:22:08 -0400

    Added a bunch of files to the github.

---
+++ homescripts.js
@@ -0,0 +1,11 @@
+document.getElementById("Qlbutton").onclick=function(){
+    location.href="http://127.0.0.1:5500/frontend/Q1.html"
+};
+
+
+//the answers to the multiple choice questions
+var options=["1", "2", "3"],
+ tot=answers.length;
+function getValue(radioName){
+    var radios=getElementsByName(radioName);
+}
