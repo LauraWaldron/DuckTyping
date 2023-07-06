aaac08f4145b4c52c02c5d594f3ba95dacbb022f
LauraWaldron <lwaldron2021@fau.edu>
2023-07-06 15:08:13 -0400

    sds

--- homescripts.js
+++ homescripts.js
@@ -1,3 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const form=document.getElementById("quiz-form");
+    form.addEventListener("submit", function(event){
+        event.preventDefault();  //prevent form submission
+        const formData=new FormData(form);
+        //get the answers for the questions
+        const answers={
+            q1: formData.get("question 1"),
+            q2: formData.get("question2"),
+            q3: formData.get("question3"),
+            q4: formData.get("question4"),
+        }
+    })
+})
+
+
+
+
 document.getElementById("Qlbutton").onclick=function(){
     location.href="http://127.0.0.1:5500/frontend/Q1.html"
 };
